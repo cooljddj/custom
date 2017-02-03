1578e252b0a0d795045df4581d102a3a53632520
cooljddj <cooljddj@gmail.com>
2017-02-03 23:43:57 +1100

    Update extension.js

--- extension.js
+++ extension.js
@@ -134,7 +134,7 @@
                         API.sendChat(ch);
                     }
                 }
-            },
+            };
         // Example code for a bot command:
         bot.commands.baconCommand = {
           command: 'bacon',  // The command to be called. With the standard command literal this would be: !bacon
