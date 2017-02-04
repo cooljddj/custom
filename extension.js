c614c2f196a15edf3e77a6d01f1891cf0ce2f8f8
cooljddj <cooljddj@gmail.com>
2017-02-04 12:25:07 +1100

    Update extension.js

--- extension.js
+++ extension.js
@@ -172,8 +172,8 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: "KDG",
       language: "english",
-      chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      chatLink: "https://rawgit.com/cooljddj/source/master/lang/en.json",
+      scriptLink: "https://rawgit.com/cooljddj/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
